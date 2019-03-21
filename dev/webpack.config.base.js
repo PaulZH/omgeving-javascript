a9570fef435d3626369705230cc7fe42cdab2c2c
Benjamin Nowack <mail@bnowack.de>
2019-03-21 10:15:44 +0100

    Install and configure vue

---
+++ dev/webpack.config.base.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const fs = require('fs-extra');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+fs.emptyDir('../dist');
+
+module.exports = {
+    entry: {
+        // "VIEW-NAME": './views/VIEW-NAME.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: 'dist/',
+        filename: '[name]-bundle.js?[chunkhash:4]'
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    optimization: {
+        runtimeChunk: {
+            name: 'webpack-runtime'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|woff|woff2|ttf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]?[hash:4]',
+                            publicPath: './',
+                            emitFile: false,
+                            useRelativePath: false
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        },
+        extensions: ['.vue', '.js']
+    }
+};
---
+++ dev/webpack.config.dev.js
@@ -0,0 +1,4 @@
+let config = require('./webpack.config.base.js');
+config.mode = 'development';
+config.devtool = '#source-map';
+module.exports = config;
