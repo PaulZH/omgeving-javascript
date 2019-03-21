267c411ba0940701e3136b385be04f11e3fd98a5
Benjamin Nowack <mail@bnowack.de>
2019-03-21 13:05:08 +0100

    Enable handlebars in dev server

--- dev/app.js
+++ dev/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+let hbs = require('hbs');
+
+// configure handlebars
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
