d75930792b6d07f570df512ed82d19264f0a4645
Benjamin Nowack <mail@bnowack.de>
2019-03-21 17:00:36 +0100

    Enable hot module replacement

--- dev/webpack.config.dev.js
+++ dev/webpack.config.dev.js
@@ -1,4 +1,16 @@
 let config = require('./webpack.config.base.js');
 config.mode = 'development';
 config.devtool = '#source-map';
+config.devServer = {
+    contentBase: './dist',
+    port: 3001,
+    hot: true,
+    hotOnly: true,
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+    }
+};
+config.output.publicPath = 'http://localhost:3001/dist/';
+config.output.filename = '[name]-bundle.js?[hash:4]';
+
 module.exports = config;
