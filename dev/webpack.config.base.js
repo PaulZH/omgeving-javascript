c8d5c95084aae4f1b0b9083fc9288ba857d44bf0
Benjamin Nowack <mail@bnowack.de>
2019-04-03 00:37:33 +0200

    Fix base path in webpack config (#1)

--- dev/webpack.config.base.js
+++ dev/webpack.config.base.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
-        publicPath: 'dist/',
+        publicPath: '/dist/',
         filename: '[name].js?[chunkhash:4]'
     },
     plugins: [
